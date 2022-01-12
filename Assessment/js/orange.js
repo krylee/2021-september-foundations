1b0ef40771e54d363d280caf7cb8f06f88779b34
krylee <katie.rylee14@gmail.com>
2022-01-11 21:34:16 -0600

    commit

--- Assessment/js/orange.js
+++ Assessment/js/orange.js
@@ -1,4 +1,19 @@
 $(function () {
+    $("#about").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+$(function () {
     $("a[href='#schedule']").click(function () {
         $("#schedule").addClass("show");
     });
@@ -11,10 +26,15 @@ $(function () {
 });
 
 $(function () {
-    $("a[href='contact']").click(function () {
-        $("#contact").addclass("show");
+    $("a[href='#contact']").click(function () {
+        $("#contact").addClass("spin");
     });
 });
 
+$(function () {
+    $(".close").click(function () {
+        $("#contact").removeClass("spin");
+    });
+});
 
 
